2f67ad7a30befe75f3a5175900a850a0f54ac25b
lorenzosalti <lorenzosalti@gmail.com>
2025-07-16 15:41:52 +0200

    feat: exercise 1 demonstration

--- src/org/lessons/java/animals/Main.java
+++ src/org/lessons/java/animals/Main.java
@@ -3,5 +3,17 @@ package org.lessons.java.animals;
 public class Main {
   public static void main(String[] args) {
 
+    Cane lucky = new Cane("Woof!", "Croccantini");
+
+    Passerotto alex = new Passerotto("Cip Cip!", "Insettini");
+
+    Aquila donald = new Aquila("'Murica!", "Roditori");
+
+    Delfino flipper = new Delfino("Gihgihgihgihgihgih gih gih gih!", "Pesci");
+
+    lucky.dormi();
+    alex.verso();
+    donald.mangia();
+    flipper.verso();
   }
 }
