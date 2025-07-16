d74db463b0141e9880ad72cf77d9ed85c61b89c4
lorenzosalti <lorenzosalti@gmail.com>
2025-07-16 15:19:20 +0200

    feat: attributes, constructor and methods defined

--- src/org/lessons/java/animals/AbstractAnimal.java
+++ src/org/lessons/java/animals/AbstractAnimal.java
@@ -3,4 +3,38 @@ package org.lessons.java.animals;
 
 public abstract class AbstractAnimal {
 
+  // ATTRIBUTES
+  private String sound;
+  private String food;
+
+  // CONSTRUCTOR
+  public AbstractAnimal(String sound, String food) {
+    this.sound = sound;
+    this.food = food;
+  }
+
+  // METHODS
+  private String getSound() {
+    return this.sound;
+  }
+
+  private void setSound(String sound) {
+    this.sound = sound;
+  }
+
+  private String getFood() {
+    return this.food;
+  }
+
+  private void setFood(String food) {
+    this.food = food;
+  }
+
+  public void dormi() {
+    System.out.println("Zzz");
+  }
+
+  public abstract void verso();
+
+  public abstract void mangia();
 }
